bd269f20c2b9eabbf81fc510b0de575c3c320713
Muhammad Fahrizal <fahrizalm375@gmail.com>
2023-05-31 16:46:16 +0700

    update 31/05

---
+++ app/src/main/java/com/example/capstoneproject/data/api/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.capstoneproject.data.api
+
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface ApiService {
+
+}
--- app/src/main/java/com/example/capstoneproject/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/capstoneproject/ui/activity/SplashScreenActivity.kt
@@ -14,14 +14,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.logo.alpha = 0f
-        binding.text.alpha = 0f
         binding.logo.animate().setDuration(3000).alpha(1f).withEndAction{
-            val i = Intent(this, LoginActivity::class.java)
-            startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
-        }
-        binding.text.animate().setDuration(3000).alpha(1f).withEndAction{
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
